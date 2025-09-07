4b89ecb338311a03b9e936d829698fd981664d14
Adeel101-del <adeelalvi994@gmail.com>
2025-09-07 14:03:20 +0530

    profile

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react'
 import { NavLink, Outlet } from "react-router-dom";
-import React from "react";
 import "../style/Navbar.css";
 import { useAuth } from "../context/authContext";
 
 function Navbar() {
     const { auth } = useAuth()
+    const [profileOpen, setProfileOpen] = useState(false);
+
+  const toggleProfile = () => {
+    setProfileOpen(!profileOpen);
+  };
+
+  const closeModal = () => {
+    setProfileOpen(false);
+  }
+
     return (
         <>
             <div id="Navbar">
@@ -13,11 +23,31 @@ function Navbar() {
                 </div>
 
                 {auth ? (
-                    <div id="link-container" className="navbar-child" >
-                        <NavLink to="/profile/:username" className="">
-                            
-                        </NavLink>
+                    <div id="link-container" className="navbar-child">
+                        <span
+                            className="profile-icon-container"
+                            onClick={toggleProfile}
+                        >
+                            <img
+                                src="user-profile-icon.png"
+                                alt="user-profile-icon"
+                                className="profile-icon"
+                            />
+                        </span>
 
+                        {profileOpen && (
+                            <div className="modal-overlay" onClick={closeModal}>
+                                <div className="modal" onClick={(e) => e.stopPropagation()}>
+                                    <ul>
+                                        <li>Profile</li>
+                                        <li>Settings</li>
+                                        <li>My Products</li>
+                                        <li>Contacts</li>
+                                        <li>Add Products</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 ) : (
                     < div id="link-container" className="navbar-child">
